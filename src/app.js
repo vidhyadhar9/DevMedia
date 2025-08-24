3f8a2c5f4e3fd9a85da988272d3c2afb6b1e93d1
vatte vidhyadhar reddy <v.vidhyadharreddy9@gmail.com>
2025-08-24 14:03:21 +0530

    error handler added

--- src/app.js
+++ src/app.js
@@ -10,12 +10,27 @@ app.listen(3000, () => {
 
 
 
-const handler2 = (req, res) => {
+const handler2 = (req, res,next) => {
   // Perform additional logic here
-  res.status(200).json({ message: "Additional handler executed" });
-  console.log("handler2 executed");
+    throw new Error("Custom error from handler2"); 
+  
 };
 
+
+
 app.post("/user",handler,handler2)
+
+
+
+app.use('/', (err, req, res,next) => { //express error middle ware
+  // res.status(200).json({ message: "Root endpoint" });
+  if(err){
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+});
+
+
+
+
 
 
